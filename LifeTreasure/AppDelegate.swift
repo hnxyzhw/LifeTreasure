290a3bbe56dcae73aef4aaa5c57955fbed8ee99b
hnxyzhw <hnxyzhw611@126.com>
2017-03-15 18:25:35 +0800

    网路请求

--- LifeTreasure/AppDelegate.swift
+++ LifeTreasure/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        Thread.sleep(forTimeInterval: 1.0)//延长3秒
+        //Thread.sleep(forTimeInterval: 1.0)//延长3秒
         return true
     }
 
--- LifeTreasure/ViewController.swift
+++ LifeTreasure/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import SwiftyJSON
+
 let identifier = "cellIdentifier"
 
 
@@ -29,6 +31,36 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         super.viewDidLoad()
         
         self.view.addSubview(self.tableview)
+        
+        self.loadData()
+    }
+    
+    func loadData() {
+
+        //https://route.showapi.com/582-2?key=&needContent=1&page=&showapi_appid=32921&showapi_timestamp=&typeId=2&showapi_sign=cc5b4721bb394890b2cd75128b1189f4
+        let url:String = "https://route.showapi.com/582-2"
+        
+        let parameters: Dictionary = ["key":"", "needConten":"1", "page":"", "showapi_appid":"32917", "showapi_timestamp":"", "typeId":"2", "showapi_sign":NSString.md5(forUpper32Bate: "needContent1showapi_appid32917typeId2cc5b4721bb394890b2cd75128b1189f4")]
+        
+        RequestServerFactory.sharedInstance.getRequest(urlString: url, params: parameters, success: { (json)-> Void in
+//            if (JSONSerialization.isValidJSONObject(json)){
+//                let data = try? JSONSerialization.data(withJSONObject: json, options: JSONSerialization.WritingOptions.prettyPrinted)
+//                let resultDic: NSDictionary =  NSKeyedUnarchiver.unarchiveObject(with: data!) as! NSDictionary
+//                print(resultDic)
+//            }
+            print(json)
+            
+        }, failure:{(error) -> Void in
+            print(error)
+        })
+        
+//        RequestServerFactory.sharedInstance.getRequest(urlString: "http://op.juhe.cn/onebox/weather/query", params: ["cityname":"杭州","key":"f6418f1f819c801a60ea7880ca844587"], success: {(json) -> Void in
+//            
+//            print(json)
+//            
+//        }, failure: {(error) -> Void in
+//            print(error)
+//        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -92,43 +124,43 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     }
     
     func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        print("移动cell，对数据远进行处理")
+        //print("移动cell，对数据远进行处理")
     }
     
     
     /// MARK: UITableviewDataSorucePrefetching
     func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
-         print("这个协议方法提供一个数组，这个数组提示了按着本次滑动方向，再接下去要碰到哪些indexPaths了，以UITableView为例，回调过来的这个数组为当前屏幕边缘的indexPath的接下去（上或者下）第10个开始算的indexPath，大概一次5个这样。")
+         //print("这个协议方法提供一个数组，这个数组提示了按着本次滑动方向，再接下去要碰到哪些indexPaths了，以UITableView为例，回调过来的这个数组为当前屏幕边缘的indexPath的接下去（上或者下）第10个开始算的indexPath，大概一次5个这样。")
     }
     
     func tableView(_ tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
-        print("这个协议返回的数组用于在，当我们快速滑动到某个区域后又立刻按着反方向滑动，那么原本被预估要出现的几个indexPath会被取消掉这样，这个数组就是存储被取消预估的indexPath。")
+        //print("这个协议返回的数组用于在，当我们快速滑动到某个区域后又立刻按着反方向滑动，那么原本被预估要出现的几个indexPath会被取消掉这样，这个数组就是存储被取消预估的indexPath。")
     }
     
     ///mark UITableviewDelegate
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        print("即将展示cell")
+        //print("即将展示cell")
     }
     
     func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
-        print("即将展示header")
+        //print("即将展示header")
     }
     
     
     func tableView(_ tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int) {
-        print("即将暂时footer")
+        //print("即将暂时footer")
     }
     
     func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        print("已经结束展示cell")
+        //print("已经结束展示cell")
     }
     
     func tableView(_ tableView: UITableView, didEndDisplayingHeaderView view: UIView, forSection section: Int) {
-        print("已经结束展示header")
+        //print("已经结束展示header")
     }
     
     func tableView(_ tableView: UITableView, didEndDisplayingFooterView view: UIView, forSection section: Int) {
-        print("已经结束展示footer")
+        //print("已经结束展示footer")
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
